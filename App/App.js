dfb2ea915b5483fdb6724b7c8ebdf93954914fd2
ridemountainpig <lin0905580802@gmail.com>
2023-01-27 13:26:41 +0800

    Update App.js

--- App/App.js
+++ App/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import Home from './components/Home';
+import Navigator from './routes/HomeStack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
