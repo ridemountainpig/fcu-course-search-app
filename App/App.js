39eb33877a464c40f150e4381914e5592b6efc66
ridemountainpig <lin0905580802@gmail.com>
2023-01-28 12:25:17 +0800

    Update App.js

--- App/App.js
+++ App/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import Home from './components/Home';
 import Navigator from './routes/HomeStack';
 
 export default function App() {
